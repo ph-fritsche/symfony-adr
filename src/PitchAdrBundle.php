b341194eb8453018c9d3dba7d7902fd5341cf5c0
Philipp Fritsche <ph.fritsche@gmail.com>
2021-05-26 10:19:44 +0200

    refactor: remove redundant code and comments

--- src/PitchAdrBundle.php
+++ src/PitchAdrBundle.php
@@ -3,21 +3,12 @@ namespace Pitch\AdrBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Pitch\AdrBundle\DependencyInjection\PitchAdrExtension;
 use Pitch\AdrBundle\DependencyInjection\Compiler\ResponseHandlerPass;
 
-/*
- * The class needs to be named exactly as the first part of the namespace.
- */
 class PitchAdrBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new ResponseHandlerPass());
-    }
-
-    protected function getContainerExtensionClass(): string
-    {
-        return PitchAdrExtension::class;
     }
 }
