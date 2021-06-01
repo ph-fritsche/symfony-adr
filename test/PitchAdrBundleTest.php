ccab27f231fd5593d42477e9e3a72b3604efd7ab
Philipp Fritsche <ph.fritsche@gmail.com>
2021-06-01 13:48:25 +0200

    test: set environment on test Kernel for Symfony 5.3

--- test/PitchAdrBundleTest.php
+++ test/PitchAdrBundleTest.php
@@ -30,7 +30,7 @@ class PitchAdrBundleTest extends KernelTestCase
 
     protected static function getKernelClass()
     {
-        return get_class(new class('', true) extends Kernel
+        return get_class(new class('test', true) extends Kernel
         {
             public function getProjectDir()
             {
